069684db53dca0840c308aa1ab551f8b8843728b
GreyBeard42 <greybeard4200@yahoo.com>
2024-09-30 17:13:32 -0700

    Create controls.js

---
+++ javascript/flightsim2/controls.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("controls")
+
+function togglePopup() {
+  if(popup.style.visibility == 'visible') {
+    popup.style.visibility = 'hidden'
+  } else {
+    popup.style.visibility = 'visible'
+  }
+}
